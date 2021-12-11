e4718b65639bf9bf3ac228769408cfead556671c
Saul Carreon <sacarreon18@gmail.com>
2021-12-11 11:42:49 -0600

    edited text manager
    added the healthText variable and now shows visible health on screen

--- scrolling-text/Assets/Scripts/TextManager.cs
+++ scrolling-text/Assets/Scripts/TextManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextManager : MonoBehaviour
 {
+    [SerializeField] private Text healthText;
+    public int health = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class TextManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        healthText.text = health.ToString();
     }
 }
