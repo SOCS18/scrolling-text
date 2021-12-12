5272953055e65fa6aa66abaa8edebc61f0401af4
Saul Carreon <sacarreon18@gmail.com>
2021-12-11 20:19:07 -0600

    Working Health decrement system

--- scrolling-text/Assets/Scripts/ButtonManager.cs
+++ scrolling-text/Assets/Scripts/ButtonManager.cs
@@ -5,30 +5,27 @@ using UnityEngine;
 public class ButtonManager : MonoBehaviour
 {
     public TextManager textManager;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void HurtButtonPressed()
     {
         Debug.Log("Pressed Hurt Button!");
-        if (textManager.health == 0)
+        //textManager.health--;
+        int finalHealth = textManager.health - textManager.healthLost;
+
+        if (finalHealth < 0)
         {
-            textManager.health = 0;
+            finalHealth = 0;
         }
-        else
+
+        StartCoroutine(HurtOverTime(finalHealth));
+    }
+
+    IEnumerator HurtOverTime(int goalHealth)
+    {
+        while (textManager.health > goalHealth)
         {
             textManager.health--;
+            yield return new WaitForSeconds(0.1f);
         }
-        Debug.Log("Test github");
     }
 }
--- scrolling-text/Assets/Scripts/TextManager.cs
+++ scrolling-text/Assets/Scripts/TextManager.cs
@@ -7,12 +7,7 @@ public class TextManager : MonoBehaviour
 {
     [SerializeField] private Text healthText;
     public int health = 0;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int healthLost;
 
     // Update is called once per frame
     void Update()
