92447019225456b8b0143ae6c60458fa33dbfefe
Saul Carreon <sacarreon18@gmail.com>
2021-12-11 20:39:29 -0600

    Button disappearing
    made button disappear while health decreases

--- scrolling-text/Assets/Scripts/ButtonManager.cs
+++ scrolling-text/Assets/Scripts/ButtonManager.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
+    [SerializeField] private GameObject hurtButton;
     public TextManager textManager;
+    public bool healthDecreasing = false;
+
+    void Start()
+    {
+        hurtButton.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (healthDecreasing)
+        {
+            hurtButton.SetActive(false);
+        }
+        else
+        {
+            hurtButton.SetActive(true);
+        }
+    }
 
     public void HurtButtonPressed()
     {
-        Debug.Log("Pressed Hurt Button!");
-        //textManager.health--;
         int finalHealth = textManager.health - textManager.healthLost;
 
         if (finalHealth < 0)
@@ -24,8 +41,10 @@ public class ButtonManager : MonoBehaviour
     {
         while (textManager.health > goalHealth)
         {
+            healthDecreasing = true;
             textManager.health--;
             yield return new WaitForSeconds(0.1f);
         }
+        healthDecreasing = false;
     }
 }
